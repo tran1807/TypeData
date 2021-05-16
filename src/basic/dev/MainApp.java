75812c92ea529641014b97931db0114a9e8220d8
github <nhutranle59@gmail.com>
2021-05-16 08:19:24 +0700

    first commit

---
+++ src/basic/dev/MainApp.java
@@ -0,0 +1,29 @@
+package basic.dev;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//Thông tin một sinh viên
+		String ten = "Le Nhu Tran";
+		int masv = 2060091;
+		int cmnd = 192122291;
+		String tenlop = "20CDTH11";
+		String tentruong = "Truong cdcn hue";
+		Double dtb = 8.4;
+		System.out.println("Thông tin một sinh viên"+"\n"+ten+"\n"+masv+"\n"+cmnd+"\n"+tenlop+"\n"+tentruong+"\n"+dtb);
+//Thông tin công nhân
+		String ten1 = "Nguyen Van An";
+		String diachi = "Thừa Thiên Huế";
+		int number = 847788886;
+		Double hesoluong = 6.5;
+		System.out.println("Thông tin công nhân"+"\n"+ten1+"\n"+diachi+"\n"+number+"\n"+hesoluong);
+//Thông tin một chiếc xe car
+		String ten2 = "xe honda civit";
+		String hang = "hãng kia";
+		String gia = "2 tỷ";
+		String mauson = "màu đỏ";
+		System.out.println("Thông tin một chiếc xa car"+"\n"+ten2+"\n"+hang+"\n"+gia+"\n"+mauson);
+	}
+
+}
